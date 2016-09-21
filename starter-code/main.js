be92d050ee594ba32d04e249b4014890ac11e0d7
Rick <rickdean@outlook.com>
2016-09-21 15:17:21 -0400

    Update main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardOne === cardTwo) {
+alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+alert ("Sorry, Try again.");
+}
